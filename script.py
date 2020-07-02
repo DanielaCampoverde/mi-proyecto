9afb8f5d5eb9b00703071c0271096e2245d51a2a
Kevin Plazarte <kplazarte@est.ups.edu.ec>
2020-07-02 10:43:26 -0500

    Nueva version

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 def main():
         nombre = input("Cual es tu nombre")
 	print("Hola",nombre, "bienvenido a Git!")
-        print("Este archivo fue modificado por 2 personas")
 if __name__ == "__main__":
 	main()
